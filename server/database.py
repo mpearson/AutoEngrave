c46719f52d2842c3b72ccd25b83b1624745ecafd
mpearson <supermacaroni@gmail.com>
2018-04-03 01:28:22 -0700

    added beginnings of a persistence layer (sqlite + peewee)

---
+++ server/database.py
@@ -0,0 +1,44 @@
+import peewee
+
+database = peewee.SqliteDatabase("auto-engrave.db")
+
+
+class SQLiteModel(peewee.Model):
+    class Meta:
+        database = database
+
+
+class Design(SQLiteModel):
+    name = peewee.CharField()
+    description = peewee.CharField()
+    filetype = peewee.CharField()
+    file = peewee.BlobField()
+    created = peewee.DateTimeField()
+    updated = peewee.DateTimeField()
+
+
+class MachineProfile(SQLiteModel):
+    name = peewee.CharField()
+    description = peewee.CharField()
+    created = peewee.DateTimeField()
+    updated = peewee.DateTimeField()
+
+    leftRightAxis = peewee.CharField()
+    frontBackAxis = peewee.CharField()
+    verticalAxis = peewee.CharField()
+    rasterScanAxis = peewee.CharField()
+
+    offsetLeft = peewee.FloatField()
+    offsetRight = peewee.FloatField()
+    offsetBack = peewee.FloatField()
+    offsetFront = peewee.FloatField()
+    offsetBottom = peewee.FloatField()
+    offsetTop = peewee.FloatField()
+
+    maxVelocityX = peewee.FloatField()
+    maxVelocityY = peewee.FloatField()
+    maxVelocityZ = peewee.FloatField()
+
+    accelerationX = peewee.FloatField()
+    accelerationY = peewee.FloatField()
+    accelerationZ = peewee.FloatField()
--- server/setup.ps1
+++ server/setup.ps1
@@ -2,3 +2,6 @@ conda install -n root -c pscondaenvs pscondaenvs
 conda create -n AutoEngrave
 activate AutoEngrave
 pip install -r requirements.txt
+
+# conda install -c blaze sqlite3
+# conda install -c conda-forge peewee
