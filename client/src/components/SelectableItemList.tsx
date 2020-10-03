143e777c7534dd4da8f9510cad5cca14eeb205c5
Matthew Pearson <supermacaroni@gmail.com>
2020-10-02 18:19:53 -0700

    WIP

--- client/src/components/SelectableItemList.tsx
+++ client/src/components/SelectableItemList.tsx
@@ -2,18 +2,18 @@ import * as React from "react";
 import { Set } from "immutable";
 import { connect } from "react-redux";
 
-
 interface SelectableItemProps {
   itemId: number;
   onClick: (e: React.MouseEvent<HTMLElement>) => void;
   selected: boolean;
 }
 
-
 interface SelectableItemListProps {
   selectedIds: Set<number>;
+  sortedItemIds: number[];
   setSelectedIds: (selectedIds: Set<number>) => void;
   listItemConponent: React.ComponentClass<SelectableItemProps>;
+  className?: string;
 }
 
 export class SelectableItemList extends React.Component<SelectableItemListProps> {
@@ -27,77 +27,59 @@ export class SelectableItemList extends React.Component<SelectableItemListProps>
     }
   }
 
-  private onClickTask = (taskIndex: number, e: React.MouseEvent<HTMLElement>) => {
-    const { selectedIds, setSelectedIds } = this.props;
+  private onClickItem = (clickIndex: number, e: React.MouseEvent<HTMLElement>) => {
+    const { selectedIds, sortedItemIds: itemIds, setSelectedIds } = this.props;
 
-    let newSet: Set<number>;
+    let newSelection: Set<number>;
 
     if (e.shiftKey) {
       // range selection from lastSelectedIndex to taskIndex
       // (or just the current item if lastSelectedIndex is null)
       // min/max ensures that start is always before end
-      const start = this.lastSelectedIndex === null ? taskIndex : Math.min(taskIndex, this.lastSelectedIndex);
-      const end = this.lastSelectedIndex === null ? taskIndex : Math.max(taskIndex, this.lastSelectedIndex);
-      let indices: number[] = [];
-      for (let i = start; i <= end; i++)
-        indices.push(i);
+      const start = this.lastSelectedIndex === null ? clickIndex : Math.min(clickIndex, this.lastSelectedIndex);
+      const end = this.lastSelectedIndex === null ? clickIndex : Math.max(clickIndex, this.lastSelectedIndex);
+      let selectionDelta: number[] = [];
+      for (let index = start; index <= end; index++)
+        selectionDelta.push(itemIds[index]);
       // if CTRL is down, keep previous selection, otherwise ignore it
-      newSet = e.ctrlKey ? selectedIds.union(indices) : Set(indices);
+      newSelection = e.ctrlKey ? selectedIds.union(selectionDelta) : Set(selectionDelta);
     } else if (e.ctrlKey) {
       // adding/subtracting an individual item
-      if (selectedIds.has(taskIndex)) {
-        newSet = selectedIds.remove(taskIndex);
+      if (selectedIds.has(clickIndex)) {
+        newSelection = selectedIds.remove(itemIds[clickIndex]);
       } else {
-        newSet = selectedIds.add(taskIndex);
+        newSelection = selectedIds.add(itemIds[clickIndex]);
       }
-      this.lastSelectedIndex = taskIndex;
+      this.lastSelectedIndex = clickIndex;
     } else {
       // select an individual item, clearing the previous selection
-      newSet = Set([taskIndex]);
-      this.lastSelectedIndex = taskIndex;
+      newSelection = Set([clickIndex]);
+      this.lastSelectedIndex = clickIndex;
     }
 
-    setSelectedIds(newSet);
+    setSelectedIds(newSelection);
   }
 
   public render() {
-    const {
-      selectedIds,
-      // ListItemConponent,
-      // activeJob, hoverTaskIndex, selectedTasks, updateSelectedTasks,
-      // deleteTask, hoverTask, sharedTaskSettings
-    } = this.props;
+    const { sortedItemIds, selectedIds, className } = this.props;
 
     const ListItemConponent = this.props.listItemConponent;
-    let globalTaskCard: JSX.Element = null;
-    let taskCards: JSX.Element[] = null;
-    // if (activeJob) {
-    //   taskCards = activeJob.tasks.map((task, index) => (
-    //     <DraggableTaskCard
-    //       model={task}
-    //       key={index}
-    //       onDelete={() => deleteTask(index)}
-    //       onMouseOver={() => hoverTask(index)}
-    //       onMouseOut={() => hoverTask(null)}
-    //       onClick={e => this.onClickTask(index, e)}
-    //       highlight={index === hoverTaskIndex}
-    //       selected={selectedTasks.has(index)}
-    //     />
-    //   ));
-    // }
-    const selectableItems = selectedIds.map(
+
+    const selectableItems = sortedItemIds.map((itemId, index) => (
+        <ListItemConponent
+          itemId={itemId}
+          selected={selectedIds.has(itemId)}
+          onClick={e => this.onClickItem(index, e)}
+        />
+    ));
+
+    const classList = className ? [className] : [];
+    classList.push("scrollable");
 
     return (
-      <div className="job-panel">
-        <TaskEditor model={sharedTaskSettings} onUpdate={updateSelectedTasks} />
-        <section className="task-list scrollable" onClick={this.onClickEmpty}>
-          {globalTaskCard}
-          {taskCards}
-        </section>
-        <section className="task-actions">
-          <GCodeTaskButton onClick={this.appendGCodeTask} />
-        </section>
-      </div>
+      <section className={classList.join(" ")} onClick={this.onClickEmpty}>
+        {selectableItems}
+      </section>
     );
   }
 }
@@ -106,21 +88,21 @@ export const GCodeTaskButton: React.SFC<{onClick: () => void}> = props => (
   <button className="blue fas fa-plus" onClick={props.onClick} title="Add G-Code task" />
 );
 
-const mapStateToProps = (state: RootState): StateProps => ({
-  activeJob: state.workspace.activeJob,
-  hoverTaskIndex: state.workspace.hoverTaskIndex,
-  selectedTasks: state.workspace.selectedTasks,
-  sharedTaskSettings: getSharedTaskSettings(state),
-});
+// const mapStateToProps = (state: RootState): StateProps => ({
+//   activeJob: state.workspace.activeJob,
+//   hoverTaskIndex: state.workspace.hoverTaskIndex,
+//   selectedTasks: state.workspace.selectedTasks,
+//   sharedTaskSettings: getSharedTaskSettings(state),
+// });
 
-const mapDispatchToProps = {
-  // onDropDesign: addDesignToTemplate,
-  // updateJob: (job: Job) => { dispatch({ type: SET_ACTIVE_JOB, job }); },
-  updateSelectedTasks: actions.updateSelectedTasks,
-  deleteTask: actions.deleteTask,
-  hoverTask: actions.hoverTask,
-  setTaskSelection: actions.setTaskSelection,
-  appendTask: actions.appendTask,
-};
+// const mapDispatchToProps = {
+//   // onDropDesign: addDesignToTemplate,
+//   // updateJob: (job: Job) => { dispatch({ type: SET_ACTIVE_JOB, job }); },
+//   updateSelectedTasks: actions.updateSelectedTasks,
+//   deleteTask: actions.deleteTask,
+//   hoverTask: actions.hoverTask,
+//   setTaskSelection: actions.setTaskSelection,
+//   appendTask: actions.appendTask,
+// };
 
-export const JobPanelConnected = connect(mapStateToProps, mapDispatchToProps)(JobPanel);
+// export const JobPanelConnected = connect(mapStateToProps, mapDispatchToProps)(JobPanel);
--- client/src/redux/catalog/actions.ts
+++ client/src/redux/catalog/actions.ts
@@ -8,6 +8,10 @@ export const createDesign = makeCreate<Design>(CATALOG_PREFIX);
 export const listDesigns = makeList<Design>(CATALOG_PREFIX);
 export const updateDesign = makeUpdate<Design>(CATALOG_PREFIX);
 export const deleteDesign = makeDelete<Design>(CATALOG_PREFIX);
+
+// TODO: get rid of this broken shit and do like addSelection() and removeSelection() or something
+// right now the DesignCatalog component is calling this with a single ID and the ctrl key status,
+// when it needs to instead be deciding whether to add or remove, and sending the changed ID set??
 export const selectDesign = (selectedIds: Set<number>): CatalogAction => ({
   type: SET_SELECTED_DESIGNS, selectedIds
 });
